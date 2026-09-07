9d3e37111bb00da3f38b306b6b79d283cfdda0dc
nuwagabavictor <nuwagabavictor197@gmail.com>
2026-09-07 14:06:15 +0300

    Add production migration command

--- dist/Database/migrations/1785265301528-init.d.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class Init1785265301528 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
-//# sourceMappingURL=1785265301528-init.d.ts.map
--- dist/Database/migrations/1788532378657-init.d.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class Init1788532378657 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
-//# sourceMappingURL=1788532378657-init.d.ts.map
